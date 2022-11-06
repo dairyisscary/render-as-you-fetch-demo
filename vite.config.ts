fdc2cba36f5fcb35a00daf95efa8e144a49312c0
Eric Kim-Butler <eric@kimbutler.xyz>
2022-11-06 14:22:57 +0100

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve as pathResolve } from "node:path";
+import { promises } from "node:fs";
+
+import { defineConfig } from "vite";
+import ReactPlugin from "@vitejs/plugin-react";
+
+const envDir = pathResolve(__dirname, "./.env");
+
+export default defineConfig(async () => {
+  return {
+    envDir,
+    plugins: [ReactPlugin()],
+    resolve: {
+      alias: {
+        "~": pathResolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: "local.spookysoftware.dev",
+      port: 1313,
+      strictPort: true,
+      https: {
+        cert: await promises.readFile(pathResolve(envDir, "./certs/local.spookysoftware.dev.pem")),
+        key: await promises.readFile(
+          pathResolve(envDir, "./certs/local.spookysoftware.dev-key.pem"),
+        ),
+      },
+    },
+  };
+});
